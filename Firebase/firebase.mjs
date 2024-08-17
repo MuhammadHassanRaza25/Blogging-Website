de44b659f1c30ff90b0980dbfda0b6bf08d2412d
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2024-08-18 01:53:01 +0500

    Blogging Website Firebase

--- Firebase/firebase.mjs
+++ Firebase/firebase.mjs
@@ -1,18 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from  "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
- // firestore start
- import { getFirestore } from  "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
- import {collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot} from  "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
- import { getApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getStorage, ref, uploadBytes, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
+import { getFirestore } from  "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import {collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot} from  "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 
-const firebaseConfig = {
-   apiKey: "AIzaSyCeP9LOeSlGNm-PL1nEEGDyha5VfZt2SfM",
-   authDomain: "blogging-website-ec987.firebaseapp.com",
-   projectId: "blogging-website-ec987",
-   storageBucket: "blogging-website-ec987.appspot.com",
-   messagingSenderId: "382059701803",
-   appId: "1:382059701803:web:4fcbfa60b1f8d85da02c6e"
- };
+ const firebaseConfig = {
+  apiKey: "AIzaSyCeP9LOeSlGNm-PL1nEEGDyha5VfZt2SfM",
+  authDomain: "blogging-website-ec987.firebaseapp.com",
+  projectId: "blogging-website-ec987",
+  storageBucket: "blogging-website-ec987.appspot.com",
+  messagingSenderId: "382059701803",
+  appId: "1:382059701803:web:4fcbfa60b1f8d85da02c6e"
+};
 
 
  export{
@@ -27,6 +27,10 @@ const firebaseConfig = {
   deleteDoc, 
   onSnapshot,
   auth,
+  uploadBytes,
+  getStorage,
+  ref,
+  getDownloadURL
 }
 
  // Initialize Firebase
--- websiteFiles/app.js
+++ websiteFiles/app.js
@@ -1,4 +1,4 @@
-import { db,getFirestore,collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot} from "../Firebase/firebase.mjs"
+import { db,getFirestore,collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot, getStorage, app, ref, uploadBytes, getDownloadURL} from "../Firebase/firebase.mjs"
 import { signOut } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { auth } from "../Firebase/firebase.mjs"
 
@@ -9,7 +9,7 @@ let searchBtn = document.getElementById('searchBtn')
 let titleInp = document.getElementById('titleInp')
 let discInp = document.getElementById('discInp')
 let nameInp = document.getElementById('nameInp')
-// let chooseFile = document.getElementById('chooseFile')
+
 
 // Logout User Start
 // let loginSignBtn = document.getElementById('loginSignBtn')
@@ -25,26 +25,51 @@ let nameInp = document.getElementById('nameInp')
 // })
 // Logout User End
 
-// set post details in Database Start
+// set post details with image in Database Start //
 let postnowBtn = document.querySelector('#postnowBtn')
+let chooseFile = document.getElementById('chooseFile')
+const storage = getStorage(app);
+
 postnowBtn?.addEventListener('click', async ()=>{
+    // // get image 
+    // // console.log(chooseFile.files[0]);
+
+    // const storageRef = ref(storage, chooseFile.files[0].name);  // yahan images ki jaga kuch name set kar sakte hain lekin name lazmi set karna hai.
+
+    // //upload file to the storage start     
+    //   uploadBytes(storageRef, chooseFile.files[0]) //uploadBytes apne sysytem se cloud ke uper file upload karne ke liye use hoya hai.
+    //   .then((snapshot) => {   
+    //     console.log('Uploaded a blob or file!');
+    //     getDownloadURL(ref(storageRef))    //getDownloadURL jahan hamari file save hai waha se link/url lene ke liye use hota hai.
+    //       .then((url) => {
+    //         console.log("url ==>", url);
+    //          var dURL = url 
+    //          return dURL
+    //       })
+    //       .catch((err) => console.log('Error'))
+    //     })
+    //      .catch((err) => console.log(err))
+    //   //upload file to the storage end
+
      try {
         const docRef = await addDoc(collection(db, "allPosts"), {
           title: titleInp.value,
           discription: discInp.value,
-          name: nameInp.value
+          name: nameInp.value,
         });
         // console.log("Document written with ID: ", docRef.id);
       } catch (e) {
         console.error("Error adding document: ", e);
     }
-    console.log('Running !');
-
+   
     titleInp.value = ''
     discInp.value = ''
     nameInp.value = ''
+
+     console.log('Running !');
+
 })
-// set post details in Database End
+// set post details with image in Database End //
 
 // get Data & show post Start
 let showPosts = document.getElementById('showPosts')
